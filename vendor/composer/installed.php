9bf9b6419231d491e3db35a6d22a68b32d71968a
Anthony5997 <anthony.rita@outlook.com>
2022-08-03 12:01:29 +0200

    test pre-commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '935d905897bb94066bb3e0b2f2d9a35ae9995139',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '935d905897bb94066bb3e0b2f2d9a35ae9995139',
+            'dev_requirement' => false,
+        ),
+        'justinrainbow/json-schema' => array(
+            'pretty_version' => '5.2.12',
+            'version' => '5.2.12.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../justinrainbow/json-schema',
+            'aliases' => array(),
+            'reference' => 'ad87d5a5ca981228e0e205c2bc7dfb8e24559b60',
+            'dev_requirement' => true,
+        ),
+    ),
+);
